e0a98c6b943e8396db1f0acac623293390adc4a0
Anahit Yeghiazaryan <anahit_yeghiazaryan@edu.aua.am>
2024-04-10 12:23:52 +0400

    Update Matrix.java

--- src/Matrices/Matrix.java
+++ src/Matrices/Matrix.java
@@ -8,7 +8,7 @@ public class Matrix {
     public Matrix(){
         this.matrix = Matrix.createMatrix();
     }
-// barev
+
     public static double[][] createMatrix(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Input the number of elements of your vector");
